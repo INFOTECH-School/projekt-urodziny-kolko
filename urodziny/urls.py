f56469b06e4266d107f4a2d1523cd72adb97dbf0
Kamil Aleksandrowicz <kamil.aleksandrowicz@infotech.edu.pl>
2025-04-03 16:58:13 +0200

    apka przed api na stronie :)

---
+++ urodziny/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import niespodzianka, Czas
+urlpatterns = [
+    path('chebanyjarnuch/<int:id>/', niespodzianka),
+
+    path('aktualna_godzina/<int:strefa_czasowa>/', Czas.as_view() ),
+]
--- urodziny/views.py
+++ urodziny/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from .models import Solenizant
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from datetime import datetime, timedelta
 # Create your views here.
+
+class Czas(APIView):
+    def get(self, request, strefa_czasowa):
+
+        return Response({"data": datetime.now() + timedelta(hours=strefa_czasowa)}, status=200)
+
+    #def post(self,request):
+    #    pass
+    #def put(self,request):
+    #    pass
+    #def delete(self,request):
+    #    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def niespodzianka(request,id):
+    cword = Solenizant.objects.get(id=id)
+    zdjecie = ""
+    if cword.stan_upojenia == "1":
+        zdjecie = cword.zdjecie1.image.url
+    elif cword.stan_upojenia == "2":
+        zdjecie = cword.zdjecie2.image.url
+    elif cword.stan_upojenia == "3":
+        zdjecie = cword.zdjecie3.image.url
+    elif cword.stan_upojenia == "4":
+        zdjecie = cword.zdjecie4.image.url
+
+    return render(request,"cos.html",context={"cword": cword, "zdjecie": zdjecie})
+
